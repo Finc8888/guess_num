6579e7d781bc99cdc0153e25b18e7e7ca73cb29a
friedfox <finc8888@gmail.com>
2024-06-15 00:29:35 +0300

    add newGame fabric

--- src/components/GameState.js
+++ src/components/GameState.js
@@ -1,7 +1,24 @@
 export default class GameState {
-  // constructor(){
-  //     console.log('GameState class is inizializated');
-  // }
+  // fabric for game creation
+  static newGame() {
+    const s = new GameState();
+    s.secret = s.randomInt(0, 100);
+    // guess should be greater
+    s.low = 0;
+    // guess should be lesser
+    s.high = 0;
+    // num of all guesses
+    s.numGuesses = 0;
+    // last guess
+    s.guess = null;
+    return s;
+  }
 
-  // fabric
+  static fromStateObject(stateObj) {
+    const s = new GameState();
+    for (let key of Object.keys(stateObj)) {
+        s[key] = stateObj(key);
+    }
+    return s;
+  }
 }
